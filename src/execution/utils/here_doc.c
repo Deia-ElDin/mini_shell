bc1972192f9d22ed92e76ece9f79013a12f0b58e
Mahmoud Elshafie <melshafi@lab3r7s7.42abudhabi.ae>
2024-05-28 17:07:40 +0400

    updates for check_for_heredoc

--- src/execution/utils/here_doc.c
+++ src/execution/utils/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: melshafi <melshafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 16:36:22 by melshafi          #+#    #+#             */
-/*   Updated: 2024/05/28 16:57:50 by melshafi         ###   ########.fr       */
+/*   Updated: 2024/05/28 17:06:09 by melshafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,17 @@ int	check_for_heredoc(t_data *data)
 {
 	t_ast	*ast;
 	char	*str;
+	char	*temp;
 
 	ast = data->ast;
 	if (ast->right->type != NODE_HEREDOC)
 		return (0);
+	temp = readline(NULL);
+	str = "";
+	while (ft_strcmp(temp, ast->right->file))
+	{
+		temp = readline(NULL);
+		str = ft_strjoin(str, temp);
+	}
 	return (1);
 }
