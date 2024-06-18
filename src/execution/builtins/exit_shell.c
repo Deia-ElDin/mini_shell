c7970d923a0ff76bf7b27502988284d9ef3b3f2d
Deia Hamad <deia.tech2021@gmail.com>
2024-06-18 21:04:40 +0400

    fixing some exit status code

--- src/execution/builtins/exit_shell.c
+++ src/execution/builtins/exit_shell.c
@@ -6,7 +6,7 @@
 /*   By: dehamad <dehamad@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 16:01:22 by dehamad           #+#    #+#             */
-/*   Updated: 2024/06/18 20:58:28 by dehamad          ###   ########.fr       */
+/*   Updated: 2024/06/18 21:03:49 by dehamad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,10 @@ void	exit_shell(t_data *data)
 		data->exit_status = 2;
 	else if (data->ast->right->cmd[1])
 	{
-		if (ft_isdigit(data->ast->right->cmd[1][0]))
-		{
-			res = ft_atoi(data->ast->right->cmd[1]);
-			if (res.error)
-				exit_code = 255;
-			data->exit_status = res.nbr;
-		}
+		res = ft_atoi(data->ast->right->cmd[1]);
+		if (res.error)
+			exit_code = 255;
+		data->exit_status = res.nbr;
 	}
 	if (data->exit_status < 0)
 		data->exit_status = 256 + (data->exit_status % 256);
